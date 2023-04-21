b2c595a69d31bdd273270890ca754e511b2de923
MattPermentier <matt.permentier@gmail.com>
2023-04-21 21:11:29 +0200

    radio buttons created for diameter and height

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,18 +6,44 @@ type SidebarProps = {
 };
 
 const Sidebar = (props: SidebarProps) => {
-  const [standingPlaces, setStandingPlaces] = useState([
-    "Sun",
-    "Partial",
-    "Shadow",
-  ]);
+  const [standingPlace, setStandingPlace] = useState<string[]>([]);
+  const [productDiameter, setProductDiameter] = useState<number[]>([]);
+  const [productHeight, setProductHeight] = useState<number[]>([]);
+
+  const setStandingValues = async () => {
+    await props.data.products?.map((product: any) => {
+      if (!standingPlace.includes(product.standingPlace)) {
+        setStandingPlace([...standingPlace, product.standingPlace]);
+      }
+    });
+  };
+  setStandingValues();
+
+  const setDiameterValues = async () => {
+    // check if diameter is unique in state and add it to the state
+    await props.data.products?.map((product: any) => {
+      if (!productDiameter.includes(product.diameter)) {
+        setProductDiameter([...productDiameter, product.diameter]);
+      }
+    });
+  };
+  setDiameterValues();
+
+  const setHeightValues = async () => {
+    await props.data.products?.map((product: any) => {
+      if (!productHeight.includes(product.height)) {
+        setProductHeight([...productHeight, product.height]);
+      }
+    });
+  };
+  setHeightValues();
 
   return (
     <div className="sidebarContainer">
       <form className="standingPlace">
         <h3>STANDPLAATS</h3>
-        {standingPlaces?.map((standingPlace: any) => (
-          <div>
+        {standingPlace?.map((standingPlace: any, index: number) => (
+          <div key={index++}>
             <input
               type="radio"
               className="weatherInput"
@@ -26,6 +52,36 @@ const Sidebar = (props: SidebarProps) => {
               onChange={() => props.weatherFilter(standingPlace)}
             />
             <label htmlFor="sunBtn">{standingPlace}</label>
+          </div>
+        ))}
+      </form>
+      <form action="" className="diameter">
+        <h3>Diameter</h3>
+        {productDiameter?.map((productDiameter: any, index: number) => (
+          <div key={index++}>
+            <input
+              type="radio"
+              className="weatherInput"
+              id="sunBtn"
+              name="standingPlace"
+              // onChange={() => props.weatherFilter(standingPlace)}
+            />
+            <label htmlFor="sunBtn">{productDiameter}</label>
+          </div>
+        ))}
+      </form>
+      <form action="" className="diameter">
+        <h3>Hoogte</h3>
+        {productHeight?.map((productHeight: any, index: number) => (
+          <div key={index++}>
+            <input
+              type="radio"
+              className="weatherInput"
+              id="sunBtn"
+              name="standingPlace"
+              // onChange={() => props.weatherFilter(standingPlace)}
+            />
+            <label htmlFor="sunBtn">{productHeight}</label>
           </div>
         ))}
       </form>
